e19031861deb523a846cf87f658e62f095a3db28
Caroline-Thies <carolinethies01@gmail.com>
2020-09-21 21:40:27 +0200

    Inital commit
    First prototype with basic movement, collision detection, and dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string inputSentence in dialogue.sentences){
+            sentences.Enqueue(inputSentence);
+        }
+        DisplayNextSenctence();
+    }
+
+    public void DisplayNextSenctence(){
+        if(sentences.Count == 0){
+            EndDialogue();
+            return;
+        }
+        string nextSentence = sentences.Dequeue();
+        dialogueText.text = nextSentence;
+    }
+
+    private void EndDialogue(){
+        animator.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange = false;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    void Update(){
+        if(isInRange){
+            if (Input.GetKeyDown(interactKey)){
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            isInRange = true;
+            Debug.Log("Player now in range");
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            isInRange = false;
+            Debug.Log("Player now out of range");
+
+        }
+    }
+}

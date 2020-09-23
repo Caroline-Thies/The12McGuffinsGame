8f5899a3f2e4d40db4fdcc1454fa05524e27f85b
Caroline-Thies <carolinethies01@gmail.com>
2020-09-23 17:14:55 +0200

    MainCamera to prefab
    MainCamera is now a prefab
    Adds Script CameraController, replaces FollowPlayer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+         DontDestroyOnLoad(transform.gameObject);
     }
 
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer(){
         Vector3 temp = transform.position; //store current own position
         temp.x = playerTransform.position.x;
         temp.y = playerTransform.position.y;

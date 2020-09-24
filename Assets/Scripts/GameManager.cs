a36b9970d476717d03ef847f73ff72a08166c7d6
Draconia27 <67484224+Draconia27@users.noreply.github.com>
2020-09-24 20:35:31 +0200

    Inventory...halfly working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.UI;
 
 public class GameManager : MonoBehaviour
 {
    public static GameManager instance;
    public bool isPaused;
    
- 
-   
+ public List<Item> items = new List<Item>();
+ public List<int> itemNumbers = new List<int>();
+  public GameObject[] slots;
+  
+  
    
    public void Awake()
    {
@@ -28,6 +32,24 @@ public class GameManager : MonoBehaviour
 	   }
 	}
 	   
+private void Start()
+{
+	DisplayItems();
+}
 
+private void DisplayItems()
+{
+	for(int i = 0; i < items.Count; i++)
+	{
+		slots[i].transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+		slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemSprite;
+		
+		slots[i].transform.GetChild(1).GetComponent<Text>().color = new Color(1, 1, 1, 0.5f);
+		slots[i].transform.GetChild(1).GetComponent<Text>().text = itemNumbers[i].ToString();
+		
+		slots[i].transform.GetChild(2).gameObject.SetActive(true);
+		Debug.Log(items[i].itemName);
+	}
+}
 }
 

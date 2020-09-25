b04cebbb56911ec92bdffaa09e881a7478586ec6
Caroline-Thies <carolinethies01@gmail.com>
2020-09-25 18:05:53 +0200

    First part of Lovelace timeline

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    public Animator animator;
+}

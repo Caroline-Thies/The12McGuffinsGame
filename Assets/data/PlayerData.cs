08b0ce451070858ce968b68ed7e54c4a6b8bf4a8
Caroline-Thies <carolinethies01@gmail.com>
2021-01-30 12:15:47 +0100

    add PlayerData class

---
+++ Assets/data/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    public string name;
+    public string saveLocation;
+    public int[] inventory
+
+    public PlayerData(Player player){
+        name = player.name;
+        saveLocation = player.saveLocation;
+        inventory = player.inventory;
+    }
+}

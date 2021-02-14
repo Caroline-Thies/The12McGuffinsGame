e373082885ff81e16436d58a1d4407ca1435c256
schroffl <schroffl@users.noreply.github.com>
2021-02-14 18:34:32 +0100

    Prompt the player for their name on game start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 4f;
+    public string username = "Default Username";
     public Rigidbody2D rb;
     private Vector2 movement;
     public Animator animator;

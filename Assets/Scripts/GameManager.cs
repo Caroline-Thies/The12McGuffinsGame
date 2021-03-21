5d46cb073427ed2f2d24ac123d83fb63da1e4c50
schroffl <schroffl@users.noreply.github.com>
2021-03-21 13:04:22 +0100

    Redesign the inventory system according to #57

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,12 +9,6 @@ public class GameManager : MonoBehaviour
    public static GameManager instance;
    public bool isPaused;
    
- public List<Item> items = new List<Item>();
- public List<int> itemNumbers = new List<int>();
-  public GameObject[] slots;
-  
-  
-   
    public void Awake()
    {
 	   
@@ -34,32 +28,21 @@ public class GameManager : MonoBehaviour
 	   
 private void Start()
 {
-	DisplayItems();
-
 	UserPrompt namePrompt = FindObjectOfType<InputManager>().prompt("Username");
 	PlayerController playerController = FindObjectOfType<PlayerController>();
+	InventoryManager invManager = FindObjectOfType<InventoryManager>();
+
+	invManager.Hide();
 
 	// I have no clue about C# GC, so I'm just gonna assume that it's gonna
 	// clean this up properly and we don't have to do it manually.
 	namePrompt.onSubmit.AddListener(name => {
 		playerController.username = name;
 		Debug.Log("The player has changed their name to '" + playerController.username + "'");
-	});
-}
 
-private void DisplayItems()
-{
-	for(int i = 0; i < items.Count; i++)
-	{
-		slots[i].transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
-		slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemSprite;
-		
-		slots[i].transform.GetChild(1).GetComponent<Text>().color = new Color(1, 1, 1, 0.5f);
-		slots[i].transform.GetChild(1).GetComponent<Text>().text = itemNumbers[i].ToString();
-		
-		slots[i].transform.GetChild(2).gameObject.SetActive(true);
-		Debug.Log(items[i].itemName);
-	}
+		invManager.GiveItem("wayback-machine");
+		invManager.Show();
+	});
 }
 }
 
---
+++ Assets/Scripts/UI/Inventory/InventoryManager.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour {
+
+    public List<InventoryItem> items = new List<InventoryItem>();
+
+    public Canvas canvas;
+    public GameObject slotContainer;
+    public GameObject itemSlotPrefab;
+
+    private class SlotInstance {
+        public GameObject uiSlot;
+        public InventoryItem item;
+    }
+
+    private List<SlotInstance> slotInstances = new List<SlotInstance>();
+    private HashSet<String> itemsInPossession = new HashSet<String>();
+
+    void Start() {
+        RectTransform containerTransform = (RectTransform) slotContainer.transform;
+        RectTransform slotTransform = (RectTransform) itemSlotPrefab.transform;
+
+        double rawItemsPerRow = containerTransform.rect.width / slotTransform.rect.width;
+        int itemsPerRow = (int) Math.Floor(rawItemsPerRow);
+
+        double offsetLeftRelative = (rawItemsPerRow - itemsPerRow) / 2.0f;
+        float offsetLeft = (float) (slotTransform.rect.width * offsetLeftRelative);
+
+        Vector2 oldPivot = containerTransform.pivot;
+        containerTransform.pivot = new Vector2(0, 1);
+
+        float slotWidth = slotTransform.rect.width;
+        float slotHeight = slotTransform.rect.height;
+
+        int index = 0;
+        items.ForEach((item) => {
+            GameObject slot = InitSlot(item);
+            slot.transform.SetParent(slotContainer.transform);
+
+            SlotInstance inst = new SlotInstance();
+            inst.uiSlot = slot;
+            inst.item = item;
+            slotInstances.Add(inst);
+            
+            RectTransform rect = (RectTransform) slot.transform;
+            rect.localPosition = Vector3.zero;
+            rect.localScale = Vector3.one;
+            rect.pivot = new Vector2(0, 1);
+
+            int row = index / itemsPerRow;
+            int column = index % itemsPerRow;
+            rect.localPosition = new Vector2(offsetLeft + column * slotWidth, -slotHeight * row);
+
+            index += 1;
+        });
+
+        containerTransform.pivot = oldPivot;
+        updateSlots();
+    }
+
+    public void GiveItem(string identifier) {
+        checkItemIdentifier(identifier);
+        itemsInPossession.Add(identifier);
+        updateSlots();
+    }
+
+    public void TakeItem(string identifier) {
+        checkItemIdentifier(identifier);
+        itemsInPossession.Remove(identifier);
+        updateSlots();
+    }
+
+    public bool HasItem(string identifier) {
+        checkItemIdentifier(identifier);
+        return itemsInPossession.Contains(identifier);
+    }
+
+    public void Show() {
+        canvas.enabled = true;
+    }
+
+    public void Hide() {
+        canvas.enabled = false;
+    }
+
+    public bool IsShown() {
+        return canvas.enabled;
+    }
+
+    private GameObject InitSlot(InventoryItem item) {
+        GameObject slot = Instantiate(itemSlotPrefab);
+
+        GameObject imageObj = slot.transform.Find("ItemSprite").gameObject;
+        Image image = imageObj.GetComponent<Image>();
+        image.sprite = item.sprite;
+
+        return slot;
+    }
+
+    private void checkItemIdentifier(string identifier) {
+        bool found = false;
+
+        foreach (SlotInstance slotInst in slotInstances) {
+            if (slotInst.item.identifier == identifier) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            throw new Exception("No item with identifier '" + identifier + "'was found.");
+        }
+    }
+
+    private void updateSlots() {
+        slotInstances.ForEach(inst => {
+            bool isVisible = itemsInPossession.Contains(inst.item.identifier);
+
+            GameObject imageObj = inst.uiSlot.transform.Find("ItemSprite").gameObject;
+            Image image = imageObj.GetComponent<Image>();
+
+            if (isVisible) {
+                image.sprite = inst.item.sprite;
+            } else {
+                image.sprite = inst.item.hiddenSprite;
+            }
+        });
+    }
+
+}

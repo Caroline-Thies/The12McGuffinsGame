4af6e150c9cb250f8ac8535e118a8a89b75917aa
Caroline-Thies <65850423+Caroline-Thies@users.noreply.github.com>
2021-04-01 15:57:15 +0200

    Add MCGUFFIN_FOUND key event

---
+++ Assets/Scripts/UI/Dialogue/ConditionalDialogue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionalDialogue : MonoBehaviour
+{
+    public string conditionalEvent;
+    public DialogueTrigger ifTriggered;
+    public DialogueTrigger ifNotTriggered;
+
+    public void triggerDialogue()
+    {
+        bool eventTriggered = FindObjectOfType<KeyEventManager>().isEventTriggered(conditionalEvent);
+        if (eventTriggered)
+        {
+            ifTriggered.TriggerDialogue();
+        } else
+        {
+            ifNotTriggered.TriggerDialogue();
+        }
+    }
+}
--- Assets/Scripts/classes/KeyEventManager.cs
+++ Assets/Scripts/classes/KeyEventManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class KeyEventManager : MonoBehaviour
 {
-    private static List<string> triggeredEvents = new List<string>();
+    private List<string> triggeredEvents = new List<string>();
 
-    public static void triggerEvent(string eventName){
+    public void triggerEvent(string eventName){
         bool eventTriggered = isEventTriggered(eventName);
         if(!eventTriggered){
             triggeredEvents.Add(eventName);
+            Debug.Log("triggered Event" + eventName);
         }
     }
 
-    public static bool isEventTriggered(string eventName){
+    public bool isEventTriggered(string eventName){
+        bool isTriggered = triggeredEvents.Contains(eventName);
+        if (isTriggered)
+        {
+            Debug.Log("Event " + eventName + " has been triggered");
+        } else
+        {
+            Debug.Log("Event " + eventName + " has not been triggered");
+        }
         return triggeredEvents.Contains(eventName);
     }
 }

a2ea249f8537d13c5b5b3edd60adf80654b523e3
Caroline-Thies <carolinethies01@gmail.com>
2021-04-06 16:19:33 +0200

    Cleanup

---
+++ Assets/Scripts/AddInventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddInventoryItem : MonoBehaviour
+{
+    public string itemId;
+
+
+    public void AddItem(){
+        InventoryManager invManager = FindObjectOfType<InventoryManager>();
+        if (!invManager.HasItem(itemId)) {
+            invManager.GiveItem(itemId);
+        }
+    }
+}

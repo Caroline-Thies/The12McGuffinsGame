8415fe846dd67bd9fc612e0ac32fc0964355c94d
schroffl <schroffl@users.noreply.github.com>
2021-04-15 13:58:33 +0200

    Add Weapon and Armor scripts
    They derive from InventoryItem so they can easily be added to /removed from it

---
+++ Assets/Scripts/Battle/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Weapon Item", fileName="New Weapon")]
+public class Weapon : InventoryItem
+{
+    public float bluntDamage = 1;
+    public float piercingDamage = 1;
+
+    void Reset() {
+        tag = "weapon";
+    }
+
+    public float calculateDamageGiven(Armor againstArmor) {
+        if (againstArmor == null) {
+            return bluntDamage + piercingDamage;
+        }
+
+        return bluntDamage * againstArmor.bluntMultiplier + piercingDamage * againstArmor.piercingMultiplier;
+    }
+}
